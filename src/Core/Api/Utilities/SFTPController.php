2583d15a724a0b4de272f3b86ccce2de9535755e
Patrick Thimm <Patrick.Thimm@synlab.com>
2021-03-10 17:54:02 +0100

    remote file unlink reactivated

--- src/Core/Api/Utilities/SFTPController.php
+++ src/Core/Api/Utilities/SFTPController.php
@@ -130,7 +130,7 @@ class SFTPController
                 }
             }
             fclose($local);
-            // ssh2_sftp_unlink($stream, $this->homeDirectory . $remoteDir . '/' . $file);
+            ssh2_sftp_unlink($stream, $this->homeDirectory . $remoteDir . '/' . $file);
             fclose($remote);            
         }
         closedir($dir);
